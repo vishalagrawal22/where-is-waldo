72c23f2c5205534e076aa2d173a1192bff59b1f3
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-05-29 00:14:15 +0530

    Add error loging to player data functions

--- src/data/playerData.js
+++ src/data/playerData.js
@@ -10,40 +10,63 @@ import {
 import { db } from "../firebase-config";
 
 export async function saveStartTime(gameId, playerId) {
-  const pendingRef = collection(db, "games", gameId, "pending");
-  const startTime = new Date();
-  const docRef = await addDoc(pendingRef, {
-    playerId,
-    startTime,
-  });
-  return docRef.id;
+  try {
+    const pendingRef = collection(db, "games", gameId, "pending");
+    const startTime = new Date();
+    const docRef = await addDoc(pendingRef, {
+      playerId,
+      startTime,
+    });
+    return docRef.id;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
 }
 
 export async function computeScore(gameId, pendingId) {
-  const pendingDocRef = doc(db, "games", gameId, "pending", pendingId);
-  const pendingDoc = await getDoc(pendingDocRef);
-  const startTime = pendingDoc.data().startTime.toDate();
-  const currentTime = Date.now();
-  const score = currentTime - startTime;
-  return score;
+  try {
+    const pendingDocRef = doc(db, "games", gameId, "pending", pendingId);
+    const pendingDoc = await getDoc(pendingDocRef);
+    const startTime = pendingDoc.data().startTime.toDate();
+    const currentTime = Date.now();
+    const score = currentTime - startTime;
+    return score;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
 }
 
 export async function getName(playerId) {
-  const playerDocRef = doc(db, "players", playerId);
-  const playerDoc = await getDoc(playerDocRef);
-  return playerDoc.data().name;
+  try {
+    const playerDocRef = doc(db, "players", playerId);
+    const playerDoc = await getDoc(playerDocRef);
+    return playerDoc.data().name;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
 }
 
 export async function setName(playerId, name) {
-  const playerDocRef = doc(db, "players", playerId);
-  await setDoc(playerDocRef, {
-    name,
-  });
+  try {
+    const playerDocRef = doc(db, "players", playerId);
+    await setDoc(playerDocRef, {
+      name,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export async function deletePending(gameId, pendingId) {
-  const pendingDocRef = doc(db, "games", gameId, "pending", pendingId);
-  await deleteDoc(pendingDocRef);
+  try {
+    const pendingDocRef = doc(db, "games", gameId, "pending", pendingId);
+    await deleteDoc(pendingDocRef);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export async function updatePlayerScore(gameId, playerId, pendingId) {
